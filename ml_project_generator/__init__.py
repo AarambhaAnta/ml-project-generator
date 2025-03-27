d3b52aa59af46f42ceb249d5364c0fbd72bdfebf
AarambhaAnta <aarambha108@gmail.com>
2025-03-27 20:02:45 +0530

    feat: build v2.0.0 package

--- ml_project_generator/__init__.py
+++ ml_project_generator/__init__.py
@@ -2,7 +2,7 @@
 ML Project Generator - A tool to generate structured ML project templates
 """
 
-__version__ = "0.1.1"
+__version__ = "2.0.0"
 
 from .project_creator import create_ml_project_structure, create_virtual_environment
 
---
+++ test/src/data/preprocessing.py
@@ -0,0 +1,7 @@
+# Data preprocessing utilities
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Implement preprocessing steps
+    return data
